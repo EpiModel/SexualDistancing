19eaae2da92da7e57eba69db48fb33803967c9fe
Samuel Jenness <smjenness@gmail.com>
2020-09-09 18:05:52 -0400

    T2 complete

--- analysis/02-Tables.R
+++ analysis/02-Tables.R
@@ -75,8 +75,49 @@ for (ii in 1:length(scenario_set)) {
   rr <- do.call("c", rr)
   t1[[ii]] <- rr
 }
-t1 <- as.data.frame(cbind(scenario_set,
-  do.call("rbind", t1)))
+t1 <- as.data.frame(cbind(scenario_set, do.call("rbind", t1)))
 
 readr::write_csv(t1, "analysis/T1.csv")
 
+
+## Table 2
+
+scenario_set <- c("base",
+                  "ser_all_05", "ser_all_09",
+                  "ser_prep_05", "ser_prep_09",
+                  "ser_scre_05", "ser_scre_09",
+                  "ser_art_05", "ser_art_09",
+                  "ser_stitx_05", "ser_stitx_09")
+epi_vars <- c("hiv_inc", "sti_inc", "sti_gc_inc", "sti_ct_inc")
+proc_vars <- c("prep_cov", "hiv_diag", "hiv_suppr", "sti_tx")
+
+qlow <- 0.025
+qhigh <- 0.975
+
+t2 <- list()
+for (ii in 1:length(scenario_set)) {
+  rr <- list()
+  for (jj in 1:length(epi_vars)) {
+    temp <- calc_quants_ir(df, scen = scenario_set[[ii]], var = epi_vars[[jj]],
+                           t.start = int_end-4, t.end = int_end,
+                           qnt.low = qlow, qnt.high = qhigh, round = 2)
+    rr <- c(rr, temp)
+    temp <- calc_quants_ci(df, scenario_set[[ii]], var = epi_vars[[jj]],
+                           t.start = ana_beg, t.end = ana_end,
+                           qnt.low = qlow, qnt.high = qhigh, round = 1)
+    rr <- c(rr, temp)
+  }
+  for (kk in 1:length(proc_vars)) {
+    temp <- calc_quants_prev(df, scen = scenario_set[[ii]],
+                             var = proc_vars[[kk]],
+                             at = int_end,
+                             mult = 1, round = 2,
+                             qnt.low = qlow, qnt.high = qhigh)
+    rr <- c(rr, temp)
+  }
+  rr <- do.call("c", rr)
+  t2[[ii]] <- rr
+}
+t2 <- as.data.frame(cbind(scenario_set, do.call("rbind", t2)))
+
+readr::write_csv(t2, "analysis/T2.csv")
