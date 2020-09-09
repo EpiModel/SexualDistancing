e60d173fcc7ecbf4e35ca1fd7d78aa1bae0e4afe
Samuel Jenness <smjenness@gmail.com>
2020-09-09 16:39:51 -0400

    Table creator

--- analysis/02-Tables.R
+++ analysis/02-Tables.R
@@ -14,7 +14,7 @@ df <- readRDS("~/data/SexDist/df.rds")
 df <- filter(df, time >= ana_beg)
 table(df$scenario)
 
-## Table 1
+## Test outcomes
 
 scen <- "base"
 scen <- "ser_prep_09"
@@ -29,49 +29,44 @@ calc_quants_ci(df, scen = scen, var = var,
                t.start = ana_beg, t.end = ana_end,
                qnt.low = 0.25, qnt.high = 0.75, round = 1)
 
+calc_quants_prev(df, scen = scen, var = "prep_cov", at = int_end,
+                 mult = 1, round = 2,
+                 qnt.low = 0.025, qnt.high = 0.975)
+
 calc_quants_ia(df, base.scen = "base", comp.scen = scen, var = var,
                t.start = ana_beg, t.end = ana_end,
                qnt.low = 0.025, qnt.high = 0.975,
                nsims = 1000, round.nia = 1, round.pia = 1)
 
-h1 <- create_var_df(df, scen, var)
-h2 <- create_quants_df(h1, low = 0.25, high = 0.75)
-h3 <- apply_roll(h2, roll)
-plot(h3[, 1], type = "l", ylim = c(0, 5), col = 2)
-draw_quants(h3, col = adjustcolor(2, alpha.f = 0.5))
-abline(v = c(int_beg-ana_beg, int_end-ana_beg), lty = 2)
-
-h1 <- create_var_df(df, scen = "base", var)
-h2 <- create_quants_df(h1, low = 0.25, high = 0.75)
-h3 <- apply_roll(h2, roll)
-lines(h3[, 1], type = "l", ylim = c(0, 3), col = 4)
-draw_quants(h3, col = adjustcolor(4, alpha.f = 0.5))
-# abline(v = c(int_beg-ana_beg, int_end-ana_beg), lty = 2)
-
-
 
 ## Table 1
 
-load("data/sim.n1000.rda")
-sim.base <- sim
-ref <- epi_stats(sim.base, otable = 1)
-ref
+scenario_set <- c("base",
+                  "net_all_05", "net_all_09",
+                  "net_casl_05", "net_casl_09", "net_casl_1",
+                  "net_ot_05", "net_ot_09", "net_ot_1")
+epi_vars <- c("hiv_inc", "sti_inc", "sti_gc_inc", "sti_ct_inc")
+proc_vars <- c("deg_main", "deg_casl", "deg_inst")
 
-cf.sims <- 1001:1012
-doParallel::registerDoParallel(parallel::detectCores())
-t1set <- foreach(i = 1:length(cf.sims)) %dopar% {
-  fn <- list.files(path = "data/",
-                   pattern = paste0("n", as.character(cf.sims[i])), full.names = TRUE)
-  load(fn)
-  sim.comp <- sim
-  epi_stats(sim.base, sim.comp, otable = 1)
+t1 <- list()
+for (ii in 1:length(scenario_set)) {
+  rr <- list()
+  for (jj in 1:length(epi_vars)) {
+    rr[[jj]] <- calc_quants_ir(df, scen = scenario_set[[ii]], var = epi_vars[[jj]],
+                               t.start = int_end-4, t.end = int_end,
+                               qnt.low = 0.025, qnt.high = 0.975)
+  }
+  for (kk in 1:length(proc_vars)) {
+    rr[[length(epi_vars)+kk]] <- calc_quants_prev(df, scen = scenario_set[[ii]],
+                                                  var = proc_vars[[kk]],
+                                                  at = int_end,
+                                                  mult = 1, round = 2,
+                                                  qnt.low = 0.025, qnt.high = 0.975)
+  }
+  rr <- do.call("c", rr)
+  t1[[ii]] <- rr
 }
-doParallel::stopImplicitCluster()
+do.call("rbind", t1)
 
-t1set <- do.call("rbind", t1set)
-t1 <- full_join(ref, t1set)
-t1 <- add_column(t1, scenario = 1000:1012, .before = 1)
-t1
-
-write_csv(t1, "data/T1.csv")
+write_csv(t1, "analysis/data/T1.csv")
 
